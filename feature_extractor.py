292193ed77b251e04328a93c029aa32fe2bea715
w1gns97 <109637008+w1gns97@users.noreply.github.com>
2026-05-18 12:50:38 +0900

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,18 +3,19 @@ from urllib.parse import urlparse
 import tldextract
 
 def extract_features(url):
+
     parsed = urlparse(url)
     domain = parsed.netloc
 
-    features = {
-        'having_IPhaving_IP_Address': -1 if re.match(r'\d+\.\d+\.\d+\.\d+', domain) else 1,
-        'URLURL_Length': -1 if len(url) >= 75 else 1,
-        'Shortining_Service': -1 if any(s in url for s in ['bit.ly', 'tinyurl.com']) else 1,
-        'having_At_Symbol': -1 if '@' in url else 1,
-        'double_slash_redirecting': -1 if url.rfind('//') > 7 else 1,
-        'Prefix_Suffix': -1 if '-' in domain else 1,
-        'having_Sub_Domain': -1 if len(tldextract.extract(url).subdomain.split('.')) > 1 else 1,
-        'SSLfinal_State': 1 if parsed.scheme == 'https' else -1
-    }
+    features = {}
+
+    features['having_IPhaving_IP_Address'] = -1 if re.match(r'\\d+\\.\\d+\\.\\d+\\.\\d+', domain) else 1
+    features['URLURL_Length'] = -1 if len(url) >= 75 else 1
+    features['Shortining_Service'] = -1 if any(s in url for s in ['bit.ly', 'tinyurl']) else 1
+    features['having_At_Symbol'] = -1 if '@' in url else 1
+    features['double_slash_redirecting'] = -1 if url.rfind('//') > 7 else 1
+    features['Prefix_Suffix'] = -1 if '-' in domain else 1
+    features['having_Sub_Domain'] = -1 if len(tldextract.extract(url).subdomain.split('.')) > 1 else 1
+    features['SSLfinal_State'] = 1 if parsed.scheme == 'https' else -1
 
     return features
